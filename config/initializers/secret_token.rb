68a3d80fb1a4318bf1be90377ece172ff8843f9e
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-02-08 21:35:22 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zeru::Application.config.secret_key_base = '411af5498e57537308e305a69f60a7b9a6ef5222b223cbef6ae950cdb6c0a20a11285a6c935e9579ff76b2dbc269dbddb5275599f6febe6f8fa94502a7b76d0d'
