829f1429905e95104d1763a24586797969a54dea
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-11-18 02:44:46 +0000

    Add route for memoids

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Zeru::Application.routes.draw do
   root "alpha_users#index"
   resources :alpha_users, except: [ :show ]
 
+  resources :memoids
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
