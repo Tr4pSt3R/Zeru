5689e52d741aa49b1fdd40f6145ce385fb124771
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-11-18 02:42:26 +0000

    Refactor controller test for static information

--- spec/controllers/info_spec.rb
+++ spec/controllers/info_spec.rb
@@ -1,22 +1,11 @@
 require 'spec_helper'
 
-describe InfoController do 
+RSpec.describe InfoController, type: :controller do 
   describe "GET #team" do 
     it "responds successfully with an HTTP 200 status code" do 
       get :team
-        expect(response).to be_success
-        expect(response.status).to eq(200)
+      expect(response).to be_success
+      expect(response.status).to eq(200)
     end
-
-    it "renders the index template" do 
-    	get :team
-    	expect(response).to render_template("team")
-    end
-  end
-
-  describe "GET #privacy" do 
-    it "responds successfully with an HTTP 200 status code" do
-      get :team 
-    end 
   end
 end
