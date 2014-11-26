191623ad69892b9d4a49a67f59e92f5b634ba7d7
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-11-26 06:58:26 +0000

    Refactor Cron spec

--- spec/workers/cron_spec.rb
+++ spec/workers/cron_spec.rb
@@ -2,32 +2,47 @@ require 'spec_helper'
 
 RSpec.describe 'Cron' do 
   before do 
-  	memoid = Fabricate :memoid, release_dates: [ Fabricate(:due_today) ]
-
   	Memoid.stub(:fetch_ripe_memoids)
-  	ReleaseWorker.stub(:perform_async).with([memoid.id])
+  	# ReleaseWorker.stub(:perform_async).with([memoid.id])
   end
 
   after :all do 
-  	# Timecop.unfreeze
+  	Timecop.unfreeze
+  end
+
+  describe ".routine" do 
+    it "runs every day at 8:00am" do 
+    	eight_am = Time.new.change(hour: 8)
+    	Timecop.freeze( eight_am )
+
+    	Cron.run
+
+   	expect(Memoid).to have_received(:fetch_ripe_memoids)
+   	expect(ReleaseWorker).to have_received(:perform)
+    end
   end
 
   context "nothing to deliver" do 
   	it "shouldn't call release job" do 
   	  eight_am = Time.new.change(hour: 8)
   	  Timecop.freeze( eight_am )
+
   	  Cron.run
-  	  expect(Memoid).to have_received(:fetch_ripe_memoids)
+  	  puts "Number of ripe memoids: #{Memoid.fetch_ripe_memoids}"
+
+  	  expect(Memoid).to have_received(:fetch_ripe_memoids).returns(0)
+  	  # expect(ReleaseWorker).to_not have_received(:perform)
+  	  # TODO 
+  	  # $brew install redis
+  	  # expect(Memoid.fetch_ripe_memoids.size).to eq(0)
   	end
   end
 
-  it "runs every day at 8:00am" do 
-  	eight_am = Time.new.change(hour: 8)
-  	Timecop.freeze( eight_am )
+  context "something to deliver" do 
+    memoid = Fabricate :memoid, release_dates: [ Fabricate(:due_today) ]
 
-  	Cron.run
-
- 	expect(Memoid).to have_received(:fetch_ripe_memoids)
- 	expect(ReleaseWorker).to have_received(:perform_async)
+    it "sends an email" do 
+      MemoidMailer.stub( :memoid_delivery )
+    end
   end
 end
