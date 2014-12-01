ade8733a253c9df06ee29475a906cd9ed9286ab7
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-12-01 20:30:38 +0000

    Add notification method for MemoidMailer

--- app/mailers/memoid_mailer.rb
+++ app/mailers/memoid_mailer.rb
@@ -1,3 +1,8 @@
 class MemoidMailer < ActionMailer::Base
-  # default :from => "jones.k.agyemang@gmail.com"
+  default :from => "jones.k.agyemang@gmail.com"
+
+  def notification
+  	@memoids = Memoid.due_today
+  	mail(:to => "mightyj@hotmail.co.uk" , :subject => "Memoly::Today")
+  end
 end
