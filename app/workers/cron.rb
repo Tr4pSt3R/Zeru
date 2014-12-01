2cb88fee4c3c3cec476ddf70939b6bd7aa59cfc4
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-12-01 20:29:47 +0000

    Refactor Cron#run method

--- app/workers/cron.rb
+++ app/workers/cron.rb
@@ -1,7 +1,13 @@
 class Cron
   def self.run
     if Time.now.hour == 8
-      Memoid.due_today
+      due = Memoid.due_today
+
+      binding.pry
+
+      if !due.empty?
+      	ReleaseWorker.perform_async
+      end
     end
   end
 end
