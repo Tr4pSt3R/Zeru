0cad616b58b73afd092694810632d19ca0cfa099
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-12-15 15:26:40 +0000

    Change default :from to custom email

--- app/mailers/memoid_mailer.rb
+++ app/mailers/memoid_mailer.rb
@@ -1,8 +1,8 @@
 class MemoidMailer < ActionMailer::Base
-  default :from => "sandbox8091fa7d1a544af0ae90aac56b02e963.mailgun.org"
+  default :from => "jones.k.agyemang@gmail.com"
 
   def notification
   	@memoids = Memoid.due_today
-  	mail(:to => "mightyj@hotmail.co.uk" , :subject => "Memoly::Today")
+  	email = mail to: "mightyj@hotmail.co.uk", subject: "Memoly::Today"
   end
 end
