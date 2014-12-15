bbba2358e53387b54d788d2e0b9e95b5ded1c537
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-12-15 16:11:20 +0000

    Add ability to run Cron job at anytime

--- app/workers/cron.rb
+++ app/workers/cron.rb
@@ -8,4 +8,14 @@ class Cron
       end
     end
   end
+
+  def self.run_anytime
+    due = Memoid.due_today
+
+    if !due.empty?
+      # schedule the release of due memoid(s)
+      # performed asynchronously within -/+ 5secs
+      ReleaseWorker.perform_async
+    end
+  end
 end
