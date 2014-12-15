c9936455348e5a9e065ae786dd45655a2173189c
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-12-15 14:26:28 +0000

    Make mail settings changes
      - change default :from address

--- app/mailers/memoid_mailer.rb
+++ app/mailers/memoid_mailer.rb
@@ -1,5 +1,5 @@
 class MemoidMailer < ActionMailer::Base
-  default :from => "jones.k.agyemang@gmail.com"
+  default :from => "sandbox8091fa7d1a544af0ae90aac56b02e963.mailgun.org"
 
   def notification
   	@memoids = Memoid.due_today
