95ef5915aa6199c9da2b34e1a07c8e8ab25ed11f
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-12-16 13:26:45 +0000

    Edit deployment configuration file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,8 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'zeru'
-set :repo_url, 'git@github.com:Tr4pSt3R/Zeru.git'
-
-# Setup RVM
-set :rbenv_type, :system
-set :rbenv_ruby, '2.0.0-p247'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
@@ -29,50 +23,24 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# what specs should be run before deloyment is allowed to
-# continue, see lib/capistrano/tasks/run_tests.cap
-set :tests, ["spec"]
-
 # Default value for keep_releases is 5
-set :keep_releases, 5
-
-# which config files should be copied by deploy:setup_config 
-# see documentation in lib/capistrano/tasls/setup_config.cap 
-# for details of operations
-set(:config_files, %w( database.example.yml ))
-
-# which config files should be made executable after copying
-# by deploy:setup_config
-# set(:executable_config_files, %w(
-#   unicorn_init.sh
-# ))
+# set :keep_releases, 5
 
 namespace :deploy do
-  before :starting, :set_rails_env do 
-    set :rails_env, (fetch(:rails_env) || fetch(:stage))
-  end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-    end
-    
-    # invoke 'unicorn:reload'
-  end
-
-  task :setup_config do
-    on roles(:app) do
-       #some stuff
     end
   end
 
@@ -86,4 +54,5 @@ namespace :deploy do
       # end
     end
   end
+
 end
