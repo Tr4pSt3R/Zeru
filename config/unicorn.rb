09d3a4b613c850dd389c5d04f9deed4bccfdaa8e
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-12-18 12:30:10 +0000

    Add Unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,15 @@
+# root of our Rails application
+root = "/Users/jonesagyemang/Project/Zeru"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.zeru.sock"
+
+# number of rails instances to boot up
+worker_processes 2
+
+# Restart app server, if the Rails app doesn't respond within 30secs
+timeout 30
