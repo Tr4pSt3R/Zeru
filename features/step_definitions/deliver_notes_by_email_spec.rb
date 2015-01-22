98c86b91349cccd33fbbbc645d3129e2e7b7b606
Jones Agyemang <mightyj@hotmail.co.uk>
2015-01-22 15:18:21 +0000

    Add step definition for email delivery

---
+++ features/step_definitions/deliver_notes_by_email_spec.rb
@@ -0,0 +1,19 @@
+Given(/^that I have previously added some notes$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^one of them, at least, is due for delivery today$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the note is delivered$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I check my email inbox$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see that I have a new note$/) do
+  pending # express the regexp above with the code you wish you had
+end
